18b78c355dbb28d1f0b896a43312ec8fa5e68a0f
NightFury742 <eshaan122@gmail.com>
2024-11-09 20:40:47 +0530

    DB init

---
+++ backend/db/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+const port=4444
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send(" api running");
+});
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+  });
